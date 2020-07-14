822e200503003d1c6de50e29724a20907507a0fd
Kiran Jodhani <kiran.jodhani.w@gmail.com>
2020-07-14 10:31:40 +0530

    christman

---
+++ Assets/AssetBundleModule/Editor/CreateAssetBundles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles/Android")]
+    static void BuildAllAssetBundlesForMobile()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles/Android";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+
+    //[MenuItem("Assets/Build AssetBundles/iOS")]
+    //static void BuildAllAssetBundlesForPC()
+    //{
+    //    string assetBundleDirectory = "Assets/AssetBundles/iOS";
+    //    if (!Directory.Exists(assetBundleDirectory))
+    //    {
+    //        Directory.CreateDirectory(assetBundleDirectory);
+    //    }
+    //    BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
+    //}
+}
---
+++ Assets/AssetBundleModule/Editor/PlayerPrefsDeleteEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class PlayerPrefsDeleteEditor : Editor
+{
+    [MenuItem("Shortcuts/ClearCache")]
+    static void ClearData()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Data Cleared");
+    }
+
+    [MenuItem("Shortcuts/ToogleObject #a")]
+    static void DeactivateObject()
+    {
+        GameObject obj = Selection.activeGameObject;
+        if(obj.activeSelf)
+        {
+            obj.SetActive(false);
+        }
+        else
+        {
+            obj.SetActive(true);
+        }
+    }
+
+}
